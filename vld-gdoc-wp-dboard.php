3b61b1ed6fb77dd47195f8d404803254a6911ba4
Vladimir Kuznetsov <vldnik84@gmail.com>
2018-08-18 23:19:25 +0300

    Added Google Service Auth and basic CPT functionality

--- vld-gdoc-wp-dboard.php
+++ vld-gdoc-wp-dboard.php
@@ -9,6 +9,38 @@ Author URI: http://github.com/vldnik84
 */
 
 /**
+ * Creates a custom post type
+ */
+function vld_cpt_function() {
+
+	$labels = array(
+		'name'               => __( 'Products', 'post type general name' ),
+		'singular_name'      => __( 'Product', 'post type singular name' ),
+		'add_new'            => __( 'Add New', 'book' ),
+		'add_new_item'       => __( 'Add New Product' ),
+		'edit_item'          => __( 'Edit Product' ),
+		'new_item'           => __( 'New Product' ),
+		'all_items'          => __( 'All Products' ),
+		'view_item'          => __( 'View Product' ),
+		'search_items'       => __( 'Search Products' ),
+		'not_found'          => __( 'No products found' ),
+		'not_found_in_trash' => __( 'No products found in the Trash' ),
+		'parent_item_colon'  =>     '',
+		'menu_name'          =>     'Products'
+	);
+	$args = array(
+		'labels'        => $labels,
+		'description'   => 'Holds our products and product specific data',
+		'public'        => true,
+		'menu_position' => 5,
+		'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt', 'comments' ),
+		'has_archive'   => true,
+	);
+	register_post_type( 'product', $args );
+}
+add_action( 'init', 'vld_cpt_function' );
+
+/**
  * Adds a widget to the dashboard
  */
 function vld_gdoc_wp_dboard_add_widget() {
@@ -19,30 +51,41 @@ function vld_gdoc_wp_dboard_add_widget() {
 		'vld_gdoc_wp_dboard_function'
 	);
 }
-
-/**
- * Adds action on wp_dashboard_setup hook
- */
 add_action( 'wp_dashboard_setup', 'vld_gdoc_wp_dboard_add_widget' );
 
 /**
- * Returns info from Google Doc
+ * Gets data from Google Doc Spreadsheet
  */
 function vld_gdoc_wp_dboard_function() {
 
-	require_once 'google-api-php-client-2.2.2/vendor/autoload.php';
+	require_once __DIR__ . '/google-api-php-client-2.2.2/vendor/autoload.php';
+
+	$service = new Google_Service_Sheets( google_sheet_auth() );
+
+	$sheet_id = file_get_contents(__DIR__ . '/access-data/sheet_id.txt');
+	$sheet_range = 'A1:F12';
+	//$sheet_range = 'A1:F12';
+
+	$result = $service->spreadsheets_values->get($sheet_id, $sheet_range)->getValues();
+	array_shift($result);
+	echo "<pre>";
+	print_r($result);
+}
+
+/**
+ * Performs server to server authorization with a service account
+ */
+function google_sheet_auth() {
+
+	$SERVICE_KEY = __DIR__ . '/access-data/service_key.json';
+	$SCOPES = array( Google_Service_Sheets::SPREADSHEETS_READONLY );
+
+	putenv('GOOGLE_APPLICATION_CREDENTIALS=' . $SERVICE_KEY);
 
 	$client = new Google_Client();
-	$client->setApplicationName("Client_Library_Examples");
-	$client->setDeveloperKey("YOUR_APP_KEY");
+	$client->useApplicationDefaultCredentials();
+	$client->setIncludeGrantedScopes(true);
+	$client->addScope($SCOPES);
 
-	$service = new Google_Service_Books($client);
-	$optParams = array('filter' => 'free-ebooks');
-	$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
-
-	foreach ($results as $item) {
-		echo $item['volumeInfo']['title'], "<br /> \n";
-	}
-
-	echo "Test Test Test";
+	return $client;
 }
