8fcd3d3e79fb43dfed35ccf57f6112f3ea445d6b
Vladimir Kuznetsov <vldnik84@gmail.com>
2018-08-20 00:42:06 +0300

    Fixed post generator and metabox creator

--- vld-gdoc-casino.php
+++ vld-gdoc-casino.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: GDoc Casino Dashboard Plugin
+Plugin Name: GDoc Casino Dashboard
 Plugin URI: http://github.com/vldnik84
 Description: Plugin takes data from Google Doc and shows it in Wordpress dashboard.
 Version: 0.1
@@ -46,7 +46,7 @@ function vld_gdoc_casino_add_cpt() {
 	);
 	register_post_type( 'vld_gdoc_casino_cpt', $args );
 }
-add_action( 'init', 'vld_add_cpt');
+add_action( 'init', 'vld_gdoc_casino_add_cpt');
 
 /**
  * Adds a widget to the dashboard
@@ -74,11 +74,11 @@ function vld_gdoc_casino_function() {
 	$sheet_range = 'A1:F12'; // full range is A1:F12
 
 	$result = $service->spreadsheets_values->get( $sheet_id, $sheet_range )->getValues();
-	//file_put_contents( __DIR__ . '/access-data/cache.json', json_encode($result) );
 
 	$key = array();
 	$sort_res = array();
 
+	// parsing the retrieved data
 	for ($i = 0; $i < sizeof($result); $i++) {
 		if ($i > 0) {
 			for ($j = 0; $j < sizeof($result[$i]); $j++) {
@@ -91,16 +91,27 @@ function vld_gdoc_casino_function() {
 		}
 	}
 
-	uasort($sort_res, function($a, $b) {
+	// sorting the retrieved data
+	usort($sort_res, function($a, $b) {
 		return $a['Sort order'] - $b['Sort order'];
 	});
 
-	foreach ( $sort_res as $value ) {
+	file_put_contents( __DIR__ . '/access-data/cache.json', json_encode($sort_res) );
+
+	for ($i = 0; $i < sizeof( $sort_res ); $i++ ) {
+
+		$post_title = $sort_res[$i]['Casino name'];
 		$post_args = array(
-			'post_type' => 'vld_casinos',
-			'post_title' => $value['Casino name'],
-			'post_status' => $value['Display status'] === 'y' ? 'publish' : 'private',
-			'post_content' => $value['Description']
+			'ID'           => post_exists($post_title) != 0 ? post_exists($post_title) : 0,
+			'post_type'    => 'vld_gdoc_casino_cpt',
+			'post_title'   => $post_title,
+			'post_status'  => $sort_res[$i]['Display status'] === 'y' ? 'publish' : 'private',
+			'post_content' => $sort_res[$i]['Description'],
+			'menu_order'   => $i,
+			'meta_input'   => array(
+				'Casino link'  => $sort_res[$i]['Casino link'],
+				'Casino image' => $sort_res[$i]['Casino image'],
+				'Sort order'   => $sort_res[$i]['Sort order'] )
 		);
 		wp_insert_post($post_args);
 	}
@@ -126,12 +137,15 @@ function vld_gdoc_casino_auth() {
 	return $client;
 }
 
-function vld_gdoc_casino_add_cmb( $post ) {
+/**
+ * Adds custom metabox with casino link
+ */
+function vld_gdoc_casino_add_cmb() {
 	add_meta_box(
 		'vld_gdoc_casino_link',
 		'Casino link',
 		'vld_gdoc_casino_link_function',
-		'events',
+		'vld_gdoc_casino_cpt',
 		'side',
 		'default'
 	);
@@ -139,64 +153,63 @@ function vld_gdoc_casino_add_cmb( $post ) {
 add_action( 'add_meta_boxes_vld_gdoc_casino_cpt', 'vld_gdoc_casino_add_cmb' );
 
 /**
- * Output the HTML for the metabox.
+ * Outputs the HTML for the metabox
  */
 function vld_gdoc_casino_link_function() {
 
 	global $post;
 
 	// Nonce field to validate form request came from current site
-	wp_nonce_field( basename( __FILE__ ), 'event_fields' );
+	wp_nonce_field( basename( __FILE__ ), 'vld_gdoc_casino_nonce' );
 
 	// Get the location data if it's already been entered
 	$casino_link = get_post_meta( $post->ID, 'Casino link', true );
 
 	// Output the field
 	echo '<input type="text" name="Casino link" value="' . esc_textarea( $casino_link )  . '" class="widefat">';
-
 }
-
-/**
- * Save the metabox data
- */
-function vld_gdoc_casino_link_save( $post_id, $post ) {
-
-	// Return if the user doesn't have edit permissions.
-	if ( ! current_user_can( 'edit_post', $post_id ) ) {
-		return $post_id;
-	}
-
-	// Verify this came from the our screen and with proper authorization,
-	// because save_post can be triggered at other times.
-	if ( ! isset( $_POST['Casino link'] ) || ! wp_verify_nonce( $_POST['event_fields'], basename(__FILE__) ) ) {
-		return $post_id;
-	}
-
-	// Now that we're authenticated, time to save the data.
-	// This sanitizes the data from the field and saves it into an array $events_meta.
-	$events_meta['Casino link'] = esc_textarea( $_POST['Casino link'] );
-
-	// Cycle through the $events_meta array.
-	// Note, in this example we just have one item, but this is helpful if you have multiple.
-	foreach ( $events_meta as $key => $value ) {
-
-		// Don't store custom data twice
-		if ( 'revision' === $post->post_type ) {
-			return $post_id;
-		}
-
-		if ( get_post_meta( $post_id, $key, false ) ) {
-			// If the custom field already has a value, update it.
-			update_post_meta( $post_id, $key, $value );
-		} else {
-			// If the custom field doesn't have a value, add it.
-			add_post_meta( $post_id, $key, $value );
-		}
-
-		if ( ! $value ) {
-			// Delete the meta key if there's no value
-			delete_post_meta( $post_id, $key );
-		}
-	}
-}
-add_action( 'save_post', 'vld_gdoc_casino_link_save', 1, 2 );
+//
+///**
+// * Save the metabox data
+// */
+//function vld_gdoc_casino_link_save( $post_id, $post ) {
+//
+//	// Return if the user doesn't have edit permissions.
+//	if ( ! current_user_can( 'edit_post', $post_id ) ) {
+//		return $post_id;
+//	}
+//
+//	// Verify this came from the our screen and with proper authorization,
+//	// because save_post can be triggered at other times.
+//	if ( ! isset( $_POST['Casino link'] ) || ! wp_verify_nonce( $_POST['event_fields'], basename(__FILE__) ) ) {
+//		return $post_id;
+//	}
+//
+//	// Now that we're authenticated, time to save the data.
+//	// This sanitizes the data from the field and saves it into an array $events_meta.
+//	$events_meta['Casino link'] = esc_textarea( $_POST['Casino link'] );
+//
+//	// Cycle through the $events_meta array.
+//	// Note, in this example we just have one item, but this is helpful if you have multiple.
+//	foreach ( $events_meta as $key => $value ) {
+//
+//		// Don't store custom data twice
+//		if ( 'revision' === $post->post_type ) {
+//			return $post_id;
+//		}
+//
+//		if ( get_post_meta( $post_id, $key, false ) ) {
+//			// If the custom field already has a value, update it.
+//			update_post_meta( $post_id, $key, $value );
+//		} else {
+//			// If the custom field doesn't have a value, add it.
+//			add_post_meta( $post_id, $key, $value );
+//		}
+//
+//		if ( ! $value ) {
+//			// Delete the meta key if there's no value
+//			delete_post_meta( $post_id, $key );
+//		}
+//	}
+//}
+//add_action( 'save_post', 'vld_gdoc_casino_link_save', 1, 2 );
